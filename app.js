ce1644d841a2fdfc612feded40d36bfd64fd0efc
sasham43 <sashasemail@gmail.com>
2016-06-25 14:13:30 -0400

    Hover text showing

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 $(function(){
+
+  var hoverText = '';
+
+  $(".link > a").on("mouseover", function(){
+    var linkID = $(this)[0].id;
+
+    switch (linkID) {
+      case 'mailLink':
+        hoverText = 'EMAIL';
+        break;
+      case 'gitLink':
+        hoverText = 'GITHUB';
+        break;
+      case 'inLink':
+        hoverText = 'LinkedIn';
+        break;
+      default:
+        hoverText = '<br />';
+    }
+
+    $(".hoverText").html(hoverText);
+    console.log('self hoverText:', linkID, hoverText);
+  });
+
+  $(".link > a").on("mouseleave", function(){
+    hoverText = '<br />';
+    $(".hoverText").html(hoverText);
+  });
+
   console.log('jquery loaded.');
 });
