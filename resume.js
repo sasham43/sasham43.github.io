ac56641bc8c2baf9891173aa3b086e632870542e
Sasha Kramer <sashasemail@gmail.com>
2021-02-21 18:22:30 -0500

    add work history

--- resume.js
+++ resume.js
@@ -51,13 +51,43 @@ class JobList extends LitElement {
                 company: 'TransUnion',
                 start_date: 'Jun 2019',
                 end_date: 'Current',
-                title: 'Software Engineer'
+                title: 'Software Engineer',
+                description: `Design and program user interfaces for multiple web applications (both internal and customer-facing). Program back end systems (writing API routes and database queries) to support front-end applications. Design and maintain custom backend systems in AWS to support web applications and data processing workflows.`
             },
             {
                 company: 'TruSignal',
                 start_date: 'Oct 2016',
                 end_date: 'Jun 2019',
-                title: 'Software Engineer'
+                title: 'Software Engineer',
+                description: `Design and program user interfaces for multiple web applications (both internal and customer-facing). Program back end systems (writing API routes and database queries) to support front-end applications. Design and maintain custom backend systems in AWS to support web applications and data processing workflows.`
+            },
+            {
+                company: 'RTS Solutionz',
+                start_date: 'Jan 2016',
+                end_date: 'Mar 2016',
+                title: 'A/V Programmer',
+                description: `Programmed Crestron A/V systems for 24 meeting rooms and two large multipurpose rooms. Designed and programmed user interfaces for end users and technical support personnel. Designed and implemented a custom camera tracking system for Cisco VTC platform.`
+            },
+            {
+                company: 'National Capitol Contracting',
+                start_date: 'Jan 2015',
+                end_date: 'May 2015',
+                title: 'A/V Programmer',
+                description: `Designed, programmed, and implemented audiovisual systems for connected rooms (conference rooms, auditoriums, and training centers). Developed user interfaces for both end users and technical staff. Programmed control interfaces for audio, video and teleconferencing equipment. Performed troubleshooting and maintenance on existing systems. Familiarity with both Crestron and AMX systems.`
+            },
+            {
+                company: 'National Capitol Contracting',
+                start_date: 'Apr 2012',
+                end_date: 'Jan 2015',
+                title: 'Multimedia Editor',
+                description: `Acted as an editor for a variety of transcripts, including federal government meetings, court cases, and television interviews. Generated closed captions for videos to meet accessibility standards. Led webcasts for government meetings and trainings including website creation, server creation, and videography.`
+            },
+            {
+                company: 'National Capitol Contracting',
+                start_date: 'Sep 2011',
+                end_date: 'Apr 2012',
+                title: 'Transcriber',
+                description: `Transcribed a wide variety of digital audio, from federal government meetings to Charlie Rose interviews.`
             },
         ]
         // this.message = 'Loading';
@@ -67,7 +97,8 @@ class JobList extends LitElement {
     static get styles() {
         return css`
             .job-list {
-
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
             }
             .job-entry {
                 border: solid 1px #ccc;
@@ -77,9 +108,11 @@ class JobList extends LitElement {
                     "company company"
                     "title title"
                     "start end"
+                    "description description"
                 ;
-                width: 300px;
-                margin-top: 10px;
+                // width: 300px;
+                margin: 10px;
+
                 justify-items: center;
             }
 
@@ -94,6 +127,10 @@ class JobList extends LitElement {
             }
             .end-date{
                 grid-area: end;
+            }
+            .description {
+                grid-area: description;
+                padding: 4px;
             }
 
         `
@@ -115,6 +152,9 @@ class JobList extends LitElement {
                     </div>
                     <div class="end-date">
                         ${i.end_date}
+                    </div>
+                    <div class="description">
+                        ${i.description}
                     </div>
                 </div>`)} 
             </div>
